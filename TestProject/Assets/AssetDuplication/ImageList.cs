163bb465123a8a0c08a601c90e24f70938bd1875
JK <eodnjs2998@gmail.com>
2025-08-30 14:10:28 +0900

    removed Editor, Packages, Tests, markdown. organized assemblies

--- TestProject/Assets/AssetDuplication/ImageList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ImageList", menuName = "ScriptableObjects/ImageList", order = 1)]
-public class ImageList : ScriptableObject
-{
-    public List<Texture2D> images = new List<Texture2D>();
-}
